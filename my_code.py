0dda3f1d673b0b7ab5f84f6f0f95ddb1e500fd87
rivasdelgado <24rdelgado@athenian.org>
2020-10-29 10:45:12 -0700

    day 0 and 1 python notes 10/27-29

--- my_code.py
+++ my_code.py
@@ -1,7 +1,44 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
+'''
 print('Hello world!')
 name = 'Rivas'
 print(f'Hello {name}')
 name2 = 'D Period'
 print(f'Hello {name} and {name2}')
+print(type(name))
+#use "type" to know what type of thing you typed is
+#variables: integers (int), floating point/decimal (float)
+#string (str), boolean (bool)
+print('hello ' + name + '!')
+print('Hello', name)
+
+#spaces dont work with naming a variable you must use underscores or capatilize the first letter of
+#each word
+
+height_of_tree = 5
+heightOfTree = 8.6
+why = True
+'''
+#print(f'the tree is {height_of_tree} feet tall')
+
+#user input
+#print('what is your favorite color?')
+#color = input()
+#print(f'I like {color} too')
+
+#print('do you like burritos')
+#answer = input()
+#print('cool')
+
+#food = input("what is your favorite snack? ")
+#print(f'yum! i like {food} too')
+
+#---------------------------------------------this is how you do math----------------------------------------
+print('enter a number')
+num = int(input())
+#num = int(num)
+twice = num * 2
+print(twice)
+print(type(num))
+
